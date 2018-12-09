8a80db0b9b0c78cdc99aaf95feee049370f6d818
Li, Eric <xiang.li@firstcommand.com>
2018-12-08 20:59:30 -0600

    Move to server folder

--- server/src/main/java/com/ezplan/controller/AccountController.java
+++ server/src/main/java/com/ezplan/controller/AccountController.java
@@ -3,10 +3,15 @@ package com.ezplan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.ezplan.service.AccountService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 public class AccountController {
@@ -18,6 +23,16 @@ public class AccountController {
     public String login() {
         String success = String.valueOf(accountService.login());
         return success;
+    }
+
+    @RequestMapping(value = "/cool-cars", method = RequestMethod.GET)
+    @CrossOrigin(origins = "http://localhost:4200")
+    public Collection<String> getCars() {
+        String success = String.valueOf(accountService.login());
+        List<String> cars = new ArrayList<>();
+        cars.add("AMC Gremlin");
+        cars.add("Triumph Stag");
+        return cars;
     }
 
 }
